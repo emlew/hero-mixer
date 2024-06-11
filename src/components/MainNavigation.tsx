882324c2ae9071051dd7682144028bd0a1772060
Emily Lewis <emily.lewis@rjet.com>
2024-06-11 16:18:32 -0400

    Add / and /heroes routes

---
+++ src/components/MainNavigation.tsx
@@ -0,0 +1,26 @@
+import { AppBar, Box, Button, Link } from "@mui/material";
+import { Link as RouterLink } from "react-router-dom";
+
+const MainNavigation: React.FC = () => {
+  return (
+    // TODO: style box to display across entire viewport width and use white text
+    <Box sx={{ display: { xs: "none", sm: "block" } }}>
+      <AppBar position="static">
+        <Box sx={{ display: { xs: "none", sm: "block" } }}>
+          <Button color="inherit">
+            <Link component={RouterLink} to="/">
+              Battle
+            </Link>
+          </Button>
+          <Button color="inherit">
+            <Link component={RouterLink} to="/heroes">
+              All Heroes
+            </Link>
+          </Button>
+        </Box>
+      </AppBar>
+    </Box>
+  );
+};
+
+export default MainNavigation;
