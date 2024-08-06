471f9b4a10afd5474cac25aa29723bb98fb218a0
Emily Lewis <emily.lewis@rjet.com>
2024-08-06 15:49:36 -0400

    Use general setter for claimed heroes

--- src/hooks/useClaimedHeroes.ts
+++ src/hooks/useClaimedHeroes.ts
@@ -6,23 +6,9 @@ const claimedHeroesAtom = atom<(Hero | undefined)[][]>([[], []]);
 export const useClaimedHeroes = () => {
   const [claimedHeroes, setClaimedHeroes] = useAtom(claimedHeroesAtom);
 
-  const claimHero = (hero: Hero, player: number) => {
-    const newClaimedHeroes = claimedHeroes;
-    newClaimedHeroes[player - 1].push(hero);
-    setClaimedHeroes(newClaimedHeroes);
-  };
-
-  const unclaimHero = (hero: Hero, player: number) => {
-    const newClaimedHeroes = claimedHeroes;
-    newClaimedHeroes[player - 1] = newClaimedHeroes[player - 1].filter(
-      (h) => h?.id !== hero.id
-    );
-    setClaimedHeroes(newClaimedHeroes);
-  };
-
   const claimHeroes = (heroes: (Hero | undefined)[][]) => {
     setClaimedHeroes(heroes);
   };
 
-  return { claimedHeroes, claimHero, unclaimHero, claimHeroes };
+  return { claimedHeroes, claimHeroes };
 };
